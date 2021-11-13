e41653bf917d48151a1fe8a3f2bbe573770d3ea0
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2021-11-13 14:23:39 +0100

    :fire: : remove init demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'unicorn-ng12';
-}
+export class AppComponent {}
